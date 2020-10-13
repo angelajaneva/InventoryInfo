f45e72fd358877a09a7ea33ed3fc12bd92a55acb
angelajaneva <janeva.angela@live.com>
2020-10-13 22:42:55 +0200

    JWT Authorization filter added

---
+++ src/backend/main/java/mk/gov/moepp/emi/invertoryinfo/config/JWTAuthorizationFilter.java
@@ -0,0 +1,65 @@
+package mk.gov.moepp.emi.invertoryinfo.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static mk.gov.moepp.emi.invertoryinfo.config.SecurityConstants.*;
+
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+    private final UserDetailsService userDetailsService;
+
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager, UserDetailsService userDetailsService) {
+        super(authenticationManager);
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String header = request.getHeader(HEADER_STRING);
+
+        if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = getAuthentication(request);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request){
+        String token = request.getHeader(HEADER_STRING);
+
+        if (token == null){
+            return null;
+        }
+
+        String user = JWT.require(Algorithm.HMAC512(SECRET.getBytes()))
+                .build()
+                .verify(token.replace(TOKEN_PREFIX,""))
+                .getSubject();
+
+        if (user == null) {
+            return null;
+        }
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(user);
+
+        return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+
+    }
+}
--- src/backend/main/java/mk/gov/moepp/emi/invertoryinfo/config/WebSecurityConfiguration.java
+++ src/backend/main/java/mk/gov/moepp/emi/invertoryinfo/config/WebSecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -39,6 +40,14 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .cors().and().csrf().disable().authorizeRequests()
                 .antMatchers("/css/**", "/js/**", "/logout").permitAll() //public
                 .anyRequest().authenticated();
+//                .and()
+//                .addFilter(new JWTAuthenticationFilter(authenticationManager(),userDetailsService,passwordEncoder()))
+//                .addFilter(new JWTAuthorizationFilter(authenticationManager(),userDetailsService))
+//
+//                // this disables session creation on Spring Security
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+
     }
 
     @Bean

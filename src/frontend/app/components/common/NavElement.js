f6e7e83bfe1689e21f21a9b3f6ef5847fd64dc03
ristovkiril <ristov.kiril@yahoo.com>
2020-10-27 20:22:34 +0100

    nav btn

--- src/frontend/app/components/common/NavElement.js
+++ src/frontend/app/components/common/NavElement.js
@@ -7,16 +7,19 @@ const NavElement = (props) => {
     return (
 
         <div>
-            {props.category.subcategories == null || props.category.subcategories.length == 0 ?
-                <li key={props.category.id} className={"nav-item"}>
+            {props.category.subcategories == null || props.category.subcategories.length === 0 ?
+                <li key={props.category.id} className={"nav-item nav-label p-2"}>
                     <input type={"checkbox"} id={props.category.id} name={props.category.id}/>
                     <label htmlFor={props.category.id}>{props.category.name}</label>
                 </li> :
                 <li key={props.category.id} className={"nav-item"}>
                     <input type={"checkbox"} id={props.category.id} name={props.category.id}/>
-                    <label htmlFor={props.category.id} className={"mx-auto"}>
+                    <label htmlFor={props.category.id} className={"mx-auto nav-label"}>
                         <span className="nav-label">   {props.category.name}   </span>
-                        <a className={"mx-auto"} data-toggle="collapse" href={"#collapseExample-"+props.category.id} role="button" aria-expanded="false" aria-controls={"collapseExample-"+props.category.id}><span className="fa arrow"/></a>
+                        <button className="btn ui-state-hover" style={{background: "transparent"}} type="button" data-toggle="collapse"
+                                data-target={"#collapseExample-"+props.category.id} aria-expanded="false" aria-controls={"#collapseExample-"+props.category.id}>
+                            <span className="fa arrow"/>
+                        </button>
                     </label>
                     <div>
                     <ul className={"collapse"} id={"collapseExample-"+props.category.id}>
